90dd979709f9016eadf46ecd8c8b51c9a1e16abf
Zheng Wang <zhengw1989@hotmail.com>
2016-07-29 13:10:35 +0800

    整理封装分页插件

---
+++ TaskManager/Models/Shared/PaginationViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using TaskManager.LogicEntity.Entities;
+
+namespace TaskManager.Models.Shared
+{
+    public class PaginationViewModel
+    {
+        public Func<int, string> GenerateUrlFunc { get; set; }
+
+        public IPagedList PagedList { get; set; }
+    }
+}
